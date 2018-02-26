c8223a883f767c8332c925dfcca2ad233e1e56fe
Jonathan Greenblatt <just4kerry@gmail.com>
2018-02-26 20:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function logWhisper(string) {
   }
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
-    result 
-  }
+    return `I can/'t hear you!`
+  } else if (stringto.toUpperCase === string)
+    return `YES INDEED!`
 }
   
