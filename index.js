1191b794f4b7d137666ef04260ae53423f86493b
Jonathan Greenblatt <just4kerry@gmail.com>
2018-02-26 20:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
   }
-  function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can\'t hear you!";
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === string) {
+    result 
   }
 }
   
