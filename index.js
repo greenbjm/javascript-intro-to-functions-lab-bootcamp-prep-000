5015b41acda139ac381820909b87646c520e51c3
Jonathan Greenblatt <just4kerry@gmail.com>
2018-02-26 20:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ function logWhisper(string) {
   function sayHiToGrandma(string) {
   return `I can\'t hear you!` 
   }
-  
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
